0aaa05f81e58d156ec513ad5e9bb3e3d7a2f6818
terraform <159118781+sushma933@users.noreply.github.com>
2024-02-23 22:35:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.2.4.203:9000/',
+  serverUrl: 'http://172.31.11.229:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
